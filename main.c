854b3e009d4ec2a828c57fbf1344b5a84948b945
Gal <koazgal@gmail.com>
2021-11-26 14:37:49 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include "my_mat.h"
 
-int main(){
-    printf("Hello");
+#define N 10;
+int matrix[N][N];
+
+void main() {
+    while (1) {
+        char command;
+        int num1, num2;
+        scanf("%c", &command);
+        if (command == 'A') {
+            for (int i = 0; i <9 ; i++) {
+                for (int j = 0; j <9 ; j++) {
+                    matrix[i][j]=0; // initializing the matrix to be all 0 just in case user didnt input well
+                }
+            }
+            Create_Matrix(matrix);
+        } else if (command == 'B') {
+            scanf( "%d" , "%d" , num1 , num2);
+            if(Check_Path(num1,num2,matrix) == 1){
+                printf("True\n");
+            }
+            else{
+                printf("False\n");
+            }
+        } else if (command == 'C') {
+
+        } else { //command is 'D'
+            break;
+        }
+    }
     return 0;
 }
---
+++ my_mat.h
@@ -0,0 +1,9 @@
+#ifndef UNTITLED2_MY_MATH_H
+#define UNTITLED2_MY_MATH_H
+#define V 10;
+
+void Create_Matrix(int graph [V][V]);
+int Floyd_Warshall_algorithm(int graph[V][V]);
+int Check_Path(int src, int dest,int graph[V][V]);
+
+#endif
