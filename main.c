f909ac5c28bd8671cc2c524afd02dce345fad2f5
Shauli Taragin <shaulitaragin@gmail.com>
2021-11-30 23:45:27 +0200

    hopefully yhis woorks

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include "my_mat.h"
 
 #define V 10
-int matrix[V][V];
 
-int main() {
-    while (1) {
-        char command;
-        int num1, num2;
-        scanf("%c", &command);
+int matrix[V][V];
+int num1, num2;
+
+int main(void) {
+    int breaky=1;
+    while (breaky==1) {
+        char command =getchar();
         if (command == 'A') {
             for (int i = 0; i <V ; i++) {
                 for (int j = 0; j <V ; j++) {
@@ -16,7 +17,8 @@ int main() {
                 }
             }
             Create_Matrix(matrix);
-        } else if (command == 'B') {
+        }
+        else if (command == 'B') {
             scanf( "%d %d" , &num1 , &num2);
             if(Check_Path(num1,num2,matrix)){
                 printf("True\n");
@@ -24,7 +26,8 @@ int main() {
             else{
                 printf("False\n");
             }
-        } else if (command == 'C') {
+        } 
+        else if (command == 'C') {
             scanf( "%d %d" , &num1 , &num2);
             if(Shortest_Path(num1,num2,matrix)!=0){
                 printf("%d\n" , Shortest_Path(num1,num2,matrix));
@@ -32,8 +35,9 @@ int main() {
             else{
                 printf("-1\n");
             }
-        }else if(command == 'D'){ //command is 'D'
-            break;
+        }
+        else if(command == 'D'){ //command is 'D'
+            breaky=0;
         }
     }
     return 0;
