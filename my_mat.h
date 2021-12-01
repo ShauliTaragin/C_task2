f5733724b5364d78e5c11da73fdcbb71bcf1f145
Shauli Taragin <shaulitaragin@gmail.com>
2021-12-01 10:12:30 +0200

    please work

--- my_mat.h
+++ my_mat.h
@@ -1,11 +1,10 @@
 #ifndef MY_MATH_H
-#define MY_MATH_H
-#define V 10
+    #define MY_MATH_H
+    const int V;
 
-void Create_Matrix(int graph [V][V]);
-void Floyd_Warshall_algorithm(int graph[V][V]);
-int Shortest_Path(int a, int b,int graph[V][V]);
-int Check_Path(int src, int dest,int graph[V][V]);
-int minimum(int a, int b);
-
+    void Create_Matrix(int [V][V]);
+    void Floyd_Warshall_algorithm(int [V][V]);
+    int Shortest_Path(int , int ,int [V][V]);
+    int Check_Path(int , int ,int [V][V]);
+    int minimum(int , int );
 #endif
